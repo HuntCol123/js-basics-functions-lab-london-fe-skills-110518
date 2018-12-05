fe43358ec1ca5c331bc3a4eceb6f69b945c63e59
Hunt Collings <hunt@collings.co.uk>
2018-12-05 21:01:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(someValue) {
+  let blockDistance = Math.abs(someValue - 42);
+  return blockDistance;
+}
+
+function distanceFromHqInFeet(someValue) {
+  let feetDistanceHQ = (distanceFromHqInBlocks(someValue) * 264)  
+ return feetDistanceHQ;
+    }
+    
+
+function distanceTravelledInFeet(start, destination) {
+ let distanceTravelled = Math.abs(start - destination)*264;
+ return distanceTravelled;
+}
+
+function calculatesFarePrice(start, destination) {
+  let price; 
+  if (distanceTravelledInFeet(start, destination) <= 400) {
+    price = 0;
+  }
+  return price; 
+}
